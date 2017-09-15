9e2e91afb65f96effb956737cb0a74198643b6ce
Saromase <seite.romain@hotmail.fr>
2017-09-15 09:26:52 +0200

    Update password.php

--- force/password.php
+++ force/password.php
@@ -1,11 +1,6 @@
 <?php
 $content = file_get_contents("../data/dico.txt");
 $passwords = explode("\n", $content);
-$arr = array();
-for ($i = 0; $i < count($passwords); $i++){
-    $arr[$i] = $passwords[$i];
-}
-
-echo json_encode($arr);
-// afin que la page s'arrete net la dessus.
+echo json_encode($passwords);
+// afin que la page s'arrete on met un exit.
 exit;
